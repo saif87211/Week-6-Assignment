7ef997aa78a5f27f89adec1ccc84557764e8c172
Saif Ali <saifali87211@gmail.com>
2024-10-20 21:27:11 +0530

    assignment complete

--- 1-use-memo/src/components/Assignment3.jsx
+++ 1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,9 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        return items.reduce((total,item) => total + item.value, 0);
+    },[items]);
     // Your code ends here
     return (
         <div>
